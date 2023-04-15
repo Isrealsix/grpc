ca8e681014fd8be26457f58fb6360b45e79573dd
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2023-04-15 20:01:29 +0700

    impolemented uniray calls

--- client/main.go
+++ client/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+
+	pb "github.com/isrealsix/grpc101/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NamesList{
+	// 	Names: []string{"Fred", "Billy", "Maximus"},
+	// }
+	callSayHello(client)
+}
